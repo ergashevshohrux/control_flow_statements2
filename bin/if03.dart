c68ef338b6bea93e82bd873cef857a7173b18f88
Shohrux <shohruxergashev1204@gmail.com>
2022-07-05 09:37:05 +0500

    if

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,20 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+int func(a,b,c){
+    if (a>b&&b>c){
+        return b;
+    }else if(c>b&&b>a){
+        return b;
+    }else if(b>a&&a>c){
+        return a;
+    }else if (c>a&&a>b){
+        return a;
+    }else if (b>c&&c>a){
+        return c;
+    }return c;
+
+}
 void main() {
+    print(func(13,16,12));
 }
